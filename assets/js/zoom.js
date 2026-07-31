f605beeac7bb36cd86b4cc6a64a98c41f5dfd63c
Wenjie Xu <wenjie.xu.cn@outlook.com>
2026-07-31 16:07:32 +0800

    feat(assets): ✨ 迁移 Math/Charts/Images/Media 簇到 al_math/al_charts/al_img_tools

--- assets/js/zoom.js
+++
@@ -1,8 +0,0 @@
-// Initialize medium zoom.
-$(document).ready(function () {
-  // 缓存背景颜色，避免重复读取样式
-  const bgColor = getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee";
-  medium_zoom = mediumZoom("[data-zoomable]", {
-    background: bgColor, // + 'ee' for trasparency.
-  });
-});
